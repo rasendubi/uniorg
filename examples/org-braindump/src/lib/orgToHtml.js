48338cfc84bf48d37c542b49b059573e133d1b54
Alexey Shmalko <rasen.dubi@gmail.com>
2021-01-22 20:21:46 +0200

    fix(org-braindump): allow URL parsing to fail, add logs to identify troublemakers

--- examples/org-braindump/src/lib/orgToHtml.js
+++ examples/org-braindump/src/lib/orgToHtml.js
@@ -13,7 +13,12 @@ const processor = unified()
   .use(toJson);
 
 export default async function orgToHtml(file) {
-  return await processor.process(file);
+  try {
+    return await processor.process(file);
+  } catch (e) {
+    console.error('failed to process file', file.path, e);
+    throw e;
+  }
 }
 
 /**
@@ -35,20 +40,31 @@ function extractExportSettings() {
 
 /**
  * Process each link to:
- * 1. Convert file:// links to path used by blog. file://file.org -> * /file.org
- * 2. Collect all links to file.data.links, so they can be used later * to calculate backlinks.
+ * 1. Convert file:// links to path used by blog. file://file.org -> /file.org
+ * 2. Collect all links to file.data.links, so they can be used later
+ * to calculate backlinks.
  */
 function processUrl({ url: urlString, propertyName, node, file }) {
   // next/link does not handle relative urls properly. Use file.path
   // (the slug of the file) to normalize link against.
-  let url = new URL(urlString, 'file://' + file.path);
+  try {
+    let url = new URL(urlString, 'file://' + file.path);
 
-  if (url.protocol === 'file:') {
-    let href = url.pathname.replace(/\.org$/, '');
-    node.properties[propertyName] = href;
+    if (url.protocol === 'file:') {
+      let href = url.pathname.replace(/\.org$/, '');
+      node.properties[propertyName] = href;
 
-    file.data.links = file.data.links || [];
-    file.data.links.push(href);
+      file.data.links = file.data.links || [];
+      file.data.links.push(href);
+    }
+  } catch (e) {
+    // This can happen if org file contains an invalid string, that
+    // looks like URL string (e.g., "http://example.com:port/" passes
+    // regexes, but fails to parse as URL).
+    console.warn('Failed to process URL', urlString, e);
+    // No re-throwing: the issue is not critical enough to stop
+    // processing. The document is still valid, it's just link that
+    // isn't.
   }
 }
 
