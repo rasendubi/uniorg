216daeddce8a8272b22239ac334582b655def9c0
Kevin Rangel <code@venikx.com>
2024-04-11 22:47:14 +0200

    feat: add content example for ids

--- examples/astro/src/content/config.ts
+++ examples/astro/src/content/config.ts
@@ -1,6 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
+  type: 'content',
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -9,6 +10,11 @@ const blog = defineCollection({
     date: z.coerce.date(),
     last_modified: z.coerce.date().optional(),
     hero_image: z.string().optional(),
+    metadata: z
+      .object({
+        ids: z.record(z.string(), z.string()),
+      })
+      .optional(),
   }),
 });
 
