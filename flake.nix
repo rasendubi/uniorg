6eb452c134e529bc59c205346425ac9a57877a97
Oleksii Shmalko <rasen.dubi@gmail.com>
2025-03-30 07:45:47 +0300

    chore: upgrade dependencies (#125)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Uniorg development flake.";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -11,13 +11,9 @@
       in {
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            nodejs-18_x
-            nodePackages.pnpm
-          ]
-          # turbo seems to be broken on macOS, so only add it on non-macOS
-          ++ nixpkgs.lib.optional (!pkgs.stdenv.isDarwin) pkgs.turbo;
-
-          TURBO_BINARY_PATH = if !pkgs.stdenv.isDarwin then "${pkgs.turbo}/bin/turbo" else "";
+            nodejs_22
+            nodejs_22.pkgs.pnpm
+          ];
         };
       });
 
