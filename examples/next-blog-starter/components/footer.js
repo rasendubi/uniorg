23823dd5300e3e951d7be5b7b387c9f20b618197
Oleksii Shmalko <rasen.dubi@gmail.com>
2025-03-30 15:55:25 +0300

    chore: master -> main (#133)

--- examples/next-blog-starter/components/footer.js
+++ examples/next-blog-starter/components/footer.js
@@ -17,7 +17,7 @@ export default function Footer() {
               Read Documentation
             </a>
             <a
-              href={`https://github.com/rasendubi/uniorg/tree/master/examples/${EXAMPLE_PATH}`}
+              href={`https://github.com/rasendubi/uniorg/tree/main/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
